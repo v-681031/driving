8857a83abe1ee05a1cda17fb730ee571bb23a2a2
v-681031 <vincentyu1031@gmail.com>
2019-09-25 11:44:18 +0800

    add while True loop

--- driving.py
+++ driving.py
@@ -1,16 +1,22 @@
 nationality = input('PLS input your nationality, TW or USA? ')
-if nationality == 'TW' or nationality == 'USA':
-	age = input('PLS input your age: ')
-	age = float(age)
-	if nationality == 'TW':
-		if age >= 18:
-			print('you can get driving permit in TW')
+while True:
+	if nationality == 'TW' or nationality == 'USA':
+		age = input('PLS input your age: ')
+		age = float(age)
+		if nationality == 'TW':
+			if age >= 18:
+				print('you can get driving permit in TW')
+			else:
+				print('Sorry, you must be 18-yr old in TW to get driving permit')
 		else:
-			print('Sorry, you must be 18-yr old in TW to get driving permit')
+			if age >= 16:
+				print('you can get driving permit in USA')
+			else:
+				print('Sorry, you must be 16-yr old in TW to get driving permit')
+		break
 	else:
-		if age >= 16:
-			print('you can get driving permit in USA')
-		else:
-			print('Sorry, you must be 16-yr old in TW to get driving permit')
-else:
-	print('you are neither TW nor USA citizen')
+		print('you are neither TW nor USA citizen')
+		print('PLS re-input nationality, TW or USA! or Q to quit')
+		nationality = input('PLS input your nationality, TW or USA? ')
+		if nationality == 'Q':
+			break
